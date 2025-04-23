8476686403b979504f3a425e79fb46a8e46d8c14
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-04-23 21:34:58 +0200

    Увеличение лимита

--- bot_admin/models.py
+++ bot_admin/models.py
@@ -22,7 +22,7 @@ class Recipe(models.Model):
             defaults={'recipes_given': 0}
         )
 
-        if record.recipes_given >= 3:
+        if record.recipes_given >= 33:
             return None
 
         recipe = Recipe.objects.filter(is_active=True).order_by('?').first()
